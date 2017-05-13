70975ddaa3829b8e5a7613e0e13012de39320bbb
bloodypastor <bloodypastor7@gmail.com>
2017-05-13 11:46:08 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { EventsAppComponent } from './events-app.component';
+import { EventsListComponent } from './events-list/events-list.component';
+import { EventThumbnailComponent } from './events-list/event-thumbnail/event-thumbnail.component';
+import { NavBarComponent } from './navbar/navbar.component';
 
-import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    EventsAppComponent,
+    EventsListComponent,
+    EventThumbnailComponent,
+    NavBarComponent,
   ],
   imports: [
     BrowserModule,
@@ -15,6 +21,6 @@ import { AppComponent } from './app.component';
     HttpModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
---
+++ src/app/events-list/event-thumbnail/event-thumbnail.component.ts
@@ -0,0 +1,16 @@
+import { Component, Output, Input, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'event-thumbnail',
+  templateUrl: './event-thumbnail.component.html',
+  styleUrls: ['./event-thumbnail.component.css']
+})
+export class EventThumbnailComponent {
+    @Input() event: any
+
+    getStartTimeClass() {
+      if(this.event && this.event.time === '8:00 am')
+      return ["green", 'bold']
+      return []
+    }
+}
